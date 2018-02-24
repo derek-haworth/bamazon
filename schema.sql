e40a4bd842209b4e031f7e627030ce7a6a476de1
derek-haworth <derekhaw1@gmail.com>
2018-02-23 23:17:04 -0600

    add mock data to products table schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
+-- Create bamazon table and use it
 DROP DATABASE IF EXISTS bamazon;
 CREATE database bamazon;
 
 USE bamazon;
 
+
+-- Products Table
 CREATE TABLE products (
   item_id INT NOT NULL,
   product_name VARCHAR(100) NULL,
@@ -12,7 +15,19 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
+INSERT INTO products 
+	(product_name, department_name, price, stock_quantity)
+VALUES 
+	("pillow", "home", 9.95, 39),
+    ("bowls", "kitchen", 7.25, 67),
+    ("microwave", "appliances", 129.99, 12);
 
+DROP TABLE products;  
+
+SELECT * from products;
+
+
+-- Departments Table
 CREATE TABLE departments (
 	department_id INTEGER(10) AUTO_INCREMENT NOT NULL,
     department_name VARCHAR(50) NOT NULL,
