3eb10a6db0ce11c12dd2371142f4d68c94348bf0
derek-haworth <derekhaw1@gmail.com>
2018-02-23 22:35:23 -0600

    declare npm variables

---
+++ bamazonSupervisor.js
@@ -0,0 +1,4 @@
+var connect = require('./mysql.js');
+var inquirer = require('inquirer');
+// store the inquirer prompt in a var per the npm inquirer docs
+var prompt = inquirer.createPromptModule();
