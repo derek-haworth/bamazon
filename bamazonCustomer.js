7c56b1f4368573a6ca1224595db8ba9b6d47b412
derek-haworth <derekhaw1@gmail.com>
2018-02-24 08:09:58 -0600

    fix old names in customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,7 @@ var prompt = inquirer.createPromptModule();
 			// loop through the list of items and display to the screen
 			var data_length = data.length;
 			for (var i = 0; i < data_length; i++) {
-				console.log('  ' + data[i].ItemID + ' "' + data[i].ProductName + '" FORMAT: ' + data[i].DepartmentName + ' - PRICE: $' + data[i].Price + ' - Qty: ' + data[i].StockQuantity + '\n');
+				console.log('  ' + data[i].item_id + ' "' + data[i].product_name + '" FORMAT: ' + data[i].department_name + ' - PRICE: $' + data[i].price + ' - Qty: ' + data[i].stock_quantity + '\n');
 			}
 
 			// call the selectProduct function here so that 
@@ -67,12 +67,12 @@ var prompt = inquirer.createPromptModule();
 		
 		var query_select = 'SELECT * FROM products WHERE ?';
 
-        connect.connection.query(query_select, {ItemID: selected_item.id}, function(err_select, data_select) {
+        connect.connection.query(query_select, {item_id: selected_item.id}, function(err_select, data_select) {
 
 			if (err_select) throw err_select;
             
             // check if the amount in stock is less than the selected amount
-        	if (data_select[0].StockQuantity < selected_item_int) {
+        	if (data_select[0].stock_quantity < selected_item_int) {
 
         		console.log('\nSorry, but you selected to purchase more than we have in stock. Please look over our products and make another selection.\n');
 
@@ -81,15 +81,15 @@ var prompt = inquirer.createPromptModule();
 
         	} else {
 
-        		var new_quantity = data_select[0].StockQuantity - selected_item_int;
+        		var new_quantity = data_select[0].stock_quantity - selected_item_int;
 
         		// store the price paid into a variable
-        		var total_price = data_select[0].Price * selected_item_int;
+        		var total_price = data_select[0].price * selected_item_int;
 
         		var query_update = 'UPDATE products p, departments d SET p.stock_quantity = ?, d.total_sales = d.total_sales + ? WHERE p.item_id = ? AND d.department_name = ?';
 
         		// update the Products table with the new stock_quantity for the purchased item
-        		connect.connection.query(query_update, [new_quantity, total_price, data_select[0].ItemID, data_select[0].DepartmentName], function(err_update, data_update) {
+        		connect.connection.query(query_update, [new_quantity, total_price, data_select[0].item_id, data_select[0].department_name], function(err_update, data_update) {
 
 					if (err_update) throw err_update;
 
